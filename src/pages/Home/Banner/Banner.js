135430ddbc7af4f7ed66e304bf6786bc529070b6
unknown <mdsazzadhosentamim95@gmail.com>
2022-11-24 12:59:17 +0600

    added text

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -7,8 +7,8 @@ const Banner = () => {
   <div className="hero-overlay bg-opacity-60"></div>
   <div className="hero-content text-center text-neutral-content">
     <div className="max-w-md">
-      <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-      <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <h1 className="mb-5 text-5xl font-bold">Welcome To Laptops Resale Market</h1>
+      <p className="mb-5">The importance of laptops is immense in the current digital era. The use of laptops is now increasing in every country of the world. There is no alternative to using a laptop to keep up with the modern world.</p>
       <button className="btn btn-primary">Get Started</button>
     </div>
   </div>
--- src/pages/Home/Section/Section.js
+++ src/pages/Home/Section/Section.js
@@ -3,18 +3,16 @@ import laptop3 from '../../../assets/images/laptop2.jpg';
 
 const Section = () => {
     return (
-        <div className="hero bg-base-200">
+        <div className="hero bg-base-200 mt-8 mb-8">
         <div className="hero-content flex-col lg:flex-row-reverse">
           <img
             src={laptop3}
             className="rounded-lg lg:w-1/2 shadow-2xl" alt=""
           />
           <div>
-            <h1 className="text-5xl font-bold">Box Office News!</h1>
+            <h1 className="text-5xl font-bold">Services We Provide!</h1>
             <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-              a id nisi.
+             Although the number of laptop users has increased in the current world, the price of laptops has not decreased much. So a new laptop is not possible for everyone. Keeping that in mind, we have created this website, so that people can buy used laptops at low prices. 
             </p>
            {/* <PrimaryButton>Getting Started</PrimaryButton> */}
           </div>
