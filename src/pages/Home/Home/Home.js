9112e9dbf3180223ffa3720ff3023ae544847b70
unknown <mdsazzadhosentamim95@gmail.com>
2022-11-24 12:02:56 +0600

    added extra section to home page

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import Section from '../Section/Section';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Section></Section>
         </div>
     );
 };
--- src/pages/Home/Section/Section.js
+++ src/pages/Home/Section/Section.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import laptop3 from '../../../assets/images/laptop2.jpg';
 
 const Section = () => {
     return (
-        <div>
-            
+        <div className="hero bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <img
+            src={laptop3}
+            className="rounded-lg lg:w-1/2 shadow-2xl" alt=""
+          />
+          <div>
+            <h1 className="text-5xl font-bold">Box Office News!</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+              a id nisi.
+            </p>
+           {/* <PrimaryButton>Getting Started</PrimaryButton> */}
+          </div>
         </div>
+      </div>
     );
 };
 
