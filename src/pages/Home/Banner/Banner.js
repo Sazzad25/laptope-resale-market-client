4192d5c54829adb983082c486fb9443516ab7da2
unknown <mdsazzadhosentamim95@gmail.com>
2022-11-24 11:56:36 +0600

    added banner

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -1,10 +1,18 @@
 import React from 'react';
+// import laptop2 from '../../../assets/images/laptop2.jpg';
 
 const Banner = () => {
     return (
-        <div>
-            
-        </div>
+    <div className="hero min-h-screen" style={{ backgroundImage: `url("https://cdn.thewirecutter.com/wp-content/media/2022/07/laptop-under-500-2048px-acer-1.jpg")` }}>
+  <div className="hero-overlay bg-opacity-60"></div>
+  <div className="hero-content text-center text-neutral-content">
+    <div className="max-w-md">
+      <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+      <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <button className="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
     );
 };
 
