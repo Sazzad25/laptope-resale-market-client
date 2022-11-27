c3a53d653dfa0e90cc08ffdeff83435d244f78f0
unknown <mdsazzadhosentamim95@gmail.com>
2022-11-27 22:23:15 +0600

    added sideber in dashboard

--- src/Layout/DashboardLayout.js
+++ src/Layout/DashboardLayout.js
@@ -7,19 +7,12 @@ const DashboardLayout = () => {
     <div>
       <Navbar></Navbar>
       <div className="drawer drawer-mobile">
-        <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-        <div className="drawer-content flex flex-col items-center justify-center">
-
+        <input id="dashboard-drawer" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content">
           <Outlet></Outlet>
-          <label
-            htmlFor="my-drawer-2"
-            className="btn btn-primary drawer-button lg:hidden"
-          >
-            Open drawer
-          </label>
         </div>
         <div className="drawer-side">
-          <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+          <label htmlFor="dashboard-drawer" className="drawer-overlay"></label>
           <ul className="menu p-4 w-80 bg-base-100 text-base-content">
             <li>
               <a>Sidebar Item 1</a>
