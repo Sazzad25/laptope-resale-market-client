af973978f901af606f1cc195979b3cd9e1a35a7b
unknown <mdsazzadhosentamim95@gmail.com>
2022-11-27 22:13:51 +0600

    added sidebar

--- src/Layout/DashboardLayout.js
+++ src/Layout/DashboardLayout.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "../pages/Navbar/Navbar";
 
 const DashboardLayout = () => {
-    return (
-        <div>
-            <h2>DashboardLayout Page</h2>
+  return (
+    <div>
+      <Navbar></Navbar>
+      <div className="drawer drawer-mobile">
+        <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content flex flex-col items-center justify-center">
+
+          <Outlet></Outlet>
+          <label
+            htmlFor="my-drawer-2"
+            className="btn btn-primary drawer-button lg:hidden"
+          >
+            Open drawer
+          </label>
         </div>
-    );
+        <div className="drawer-side">
+          <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+          <ul className="menu p-4 w-80 bg-base-100 text-base-content">
+            <li>
+              <a>Sidebar Item 1</a>
+            </li>
+            <li>
+              <a>Sidebar Item 2</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardLayout;
